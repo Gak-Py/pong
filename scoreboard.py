7a69d740ff7b1cccf41a897155ca5ee9a73c9e17
jipdesign-jp <jipdesigninfo@gmail.com>
2022-04-06 18:03:39 +0900

    font size bigger

--- scoreboard.py
+++ scoreboard.py
@@ -15,21 +15,21 @@ class ScoreBoard(Turtle):
     def write_score(self):
         self.clear()
         self.goto(-100, 260)
-        self.write(self.l_score, align="center", font=("Futura", 20, "italic"))
+        self.write(self.l_score, align="center", font=("Futura", 30, "italic"))
         self.goto(100, 260)
-        self.write(self.r_score, align="center", font=("Futura", 20, "italic"))
+        self.write(self.r_score, align="center", font=("Futura", 30, "italic"))
 
     def get_point_l(self):
         self.l_score += 1
         self.goto(0, 0)
-        self.write("Left Player WIN!", align="center", font=("Futura", 20, "italic"))
+        self.write("Left Player WIN!", align="center", font=("Futura", 40, "italic"))
         time.sleep(3)
         self.write_score()
 
     def get_point_r(self):
         self.r_score += 1
         self.goto(0, 0)
-        self.write("Right Player WIN!", align="center", font=("Futura", 20, "italic"))
+        self.write("Right Player WIN!", align="center", font=("Futura", 40, "italic"))
         time.sleep(3)
         self.write_score()
 
