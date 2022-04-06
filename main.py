c545219adca5b3b25fb3b53378bbb1b32c5dd40d
jipdesign-jp <jipdesigninfo@gmail.com>
2022-04-06 17:36:21 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from turtle import Screen
+from bar import Bar
+from scoreboard import ScoreBoard
+from ball import Ball
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(800,600)
+screen.title("PONG")
+
+screen.tracer(0)
+
+ball = Ball()
+score_board = ScoreBoard()
+
+bar_l = Bar(-360, 40)
+bar_r = Bar(350, 40)
+
+screen.listen()
+screen.onkey(bar_r.go_up, "[")
+screen.onkey(bar_r.go_down, "]")
+screen.onkey(bar_l.go_up, "q")
+screen.onkey(bar_l.go_down, "a")
+
+game_on = True
+while game_on:
+    time.sleep(ball.ball_speed)
+    ball.move()
+    if ball.ycor() >= 300 or ball.ycor() <= -300:
+        ball.bounce_y()
+    if ball.xcor() >= 400:
+        score_board.get_point_l()
+        ball.goto(0, 0)
+    if ball.xcor() <= -400:
+        score_board.get_point_r()
+        ball.goto(0, 0)
+    if bar_l.distance(ball) < 30 and ball.xcor() <= -340 or bar_r.distance(ball) < 30 and ball.xcor() >= 310:
+        ball.bounce_x()
+    screen.update()
+
+screen.exitonclick()
+
+
---
+++ scoreboard.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+import time
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super(ScoreBoard, self).__init__()
+        self.penup()
+        self.ht()
+        self.color("white")
+        self.l_score = 0
+        self.r_score = 0
+        self.write_score()
+
+    def write_score(self):
+        self.clear()
+        self.goto(-100, 260)
+        self.write(self.l_score, align="center", font=("Futura", 20, "italic"))
+        self.goto(100, 260)
+        self.write(self.r_score, align="center", font=("Futura", 20, "italic"))
+
+    def get_point_l(self):
+        self.l_score += 1
+        self.goto(0, 0)
+        self.write("Left Player WIN!", align="center", font=("Futura", 20, "italic"))
+        time.sleep(3)
+        self.write_score()
+
+    def get_point_r(self):
+        self.r_score += 1
+        self.goto(0, 0)
+        self.write("Right Player WIN!", align="center", font=("Futura", 20, "italic"))
+        time.sleep(3)
+        self.write_score()
+
+
